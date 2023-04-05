5c3711e778f55b23d3bfabc8de16154a91e1ad51
Lucas Baldasso <engs-lucasbaldasso@camporeal.edu.br>
2023-04-05 19:38:02 -0300

    adicionando porta clound

--- src/main.ts
+++ src/main.ts
@@ -15,6 +15,6 @@ async function bootstrap() {
     app.close();
   });
   const app = await NestFactory.create(ProdutosModule);
-  await app.listen(3000);
+  await app.listen(process.env.PORT || 3000);
 }
 bootstrap();
