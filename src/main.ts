f85faa59df7aa99ee82944ee46ef4e3ef394157c
Lucas Baldasso <engs-lucasbaldasso@camporeal.edu.br>
2023-04-05 22:54:27 -0300

    terminando configuração cors

--- src/main.ts
+++ src/main.ts
@@ -18,6 +18,8 @@ async function bootstrap() {
   const app = await NestFactory.create(ProdutosModule);
   app.use((req:any,res:any,next:any) =>{
     res.setHeader('Access-Control-Allow-Origin', '*')
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
     next();
   })
   await app.listen(process.env.PORT || 3000);
