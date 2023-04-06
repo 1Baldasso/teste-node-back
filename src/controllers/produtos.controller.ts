91ab5ca2d2702fb975af7f0ebd94fedea39bd5d7
Lucas Baldasso <engs-lucasbaldasso@camporeal.edu.br>
2023-04-05 22:20:53 -0300

    enabling CORS

--- src/controllers/produtos.controller.ts
+++ src/controllers/produtos.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Query, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import { Controller, Query, Get, Post, Body, Patch, Param, Delete, Header } from '@nestjs/common';
 import { ProdutosService } from '../services/produtos.service';
 import { CreateProdutoDto } from '../models/dto/create-produto.dto';
 import { UpdateProdutoDto } from '../models/dto/update-produto.dto';
@@ -8,11 +8,15 @@ export class ProdutosController {
   constructor(private readonly produtosService: ProdutosService) {}
 
   @Post()
+  @Header('Access-Control-Allow-Origin', 'https://mydomain.com')
+  @Header('Access-Control-Allow-Headers', 'Content-Type, Authorization')
   create(@Body() createProdutoDto: CreateProdutoDto) {
     return this.produtosService.create(createProdutoDto);
   }
 
   @Get()
+  @Header('Access-Control-Allow-Origin', 'https://mydomain.com')
+  @Header('Access-Control-Allow-Headers', 'Content-Type, Authorization')
   findAll(@Query('nome') nome: string, @Query('categoria') categoria: string) {
     if(nome || categoria)
     {
@@ -22,17 +26,23 @@ export class ProdutosController {
   }
 
   @Get(':id')
+  @Header('Access-Control-Allow-Origin', 'https://mydomain.com')
+  @Header('Access-Control-Allow-Headers', 'Content-Type, Authorization')
   findOne(@Param('id') id: string) {
     return this.produtosService.findOne(id);
   }
 
   @Patch(':id')
+  @Header('Access-Control-Allow-Origin', 'https://mydomain.com')
+  @Header('Access-Control-Allow-Headers', 'Content-Type, Authorization')
   async update(@Param('id') id: string, @Body() updateProdutoDto: UpdateProdutoDto) {
     this.produtosService.update(id, updateProdutoDto);
     return {message: "Updated", object: await this.produtosService.findOne(id)};
   }
 
   @Delete(':id')
+  @Header('Access-Control-Allow-Origin', 'https://mydomain.com')
+  @Header('Access-Control-Allow-Headers', 'Content-Type, Authorization')
   async remove(@Param('id') id: string) {
     return {message: "Object removed successfully", deleted: await this.produtosService.remove(id)};
   }
