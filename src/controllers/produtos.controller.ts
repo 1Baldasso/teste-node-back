a80aab6dbd73e5868e3122a49c0c60cb066e6324
Lucas Baldasso <engs-lucasbaldasso@camporeal.edu.br>
2023-04-05 22:22:54 -0300

    enabling CORS

--- src/controllers/produtos.controller.ts
+++ src/controllers/produtos.controller.ts
@@ -8,14 +8,16 @@ export class ProdutosController {
   constructor(private readonly produtosService: ProdutosService) {}
 
   @Post()
-  @Header('Access-Control-Allow-Origin', 'https://mydomain.com')
+  @Header('Access-Control-Allow-Origin', 'http://localhost:3001/')
+  @Header('Access-Control-Allow-Origin', 'https://teste-react-front.vercel.app/')
   @Header('Access-Control-Allow-Headers', 'Content-Type, Authorization')
   create(@Body() createProdutoDto: CreateProdutoDto) {
     return this.produtosService.create(createProdutoDto);
   }
-
+  
   @Get()
-  @Header('Access-Control-Allow-Origin', 'https://mydomain.com')
+  @Header('Access-Control-Allow-Origin', 'http://localhost:3001/')
+  @Header('Access-Control-Allow-Origin', 'https://teste-react-front.vercel.app/')
   @Header('Access-Control-Allow-Headers', 'Content-Type, Authorization')
   findAll(@Query('nome') nome: string, @Query('categoria') categoria: string) {
     if(nome || categoria)
@@ -24,24 +26,27 @@ export class ProdutosController {
     }
     return this.produtosService.findAll();
   }
-
+  
   @Get(':id')
-  @Header('Access-Control-Allow-Origin', 'https://mydomain.com')
+  @Header('Access-Control-Allow-Origin', 'http://localhost:3001/')
+  @Header('Access-Control-Allow-Origin', 'https://teste-react-front.vercel.app/')
   @Header('Access-Control-Allow-Headers', 'Content-Type, Authorization')
   findOne(@Param('id') id: string) {
     return this.produtosService.findOne(id);
   }
-
+  
   @Patch(':id')
-  @Header('Access-Control-Allow-Origin', 'https://mydomain.com')
+  @Header('Access-Control-Allow-Origin', 'http://localhost:3001/')
+  @Header('Access-Control-Allow-Origin', 'https://teste-react-front.vercel.app/')
   @Header('Access-Control-Allow-Headers', 'Content-Type, Authorization')
   async update(@Param('id') id: string, @Body() updateProdutoDto: UpdateProdutoDto) {
     this.produtosService.update(id, updateProdutoDto);
     return {message: "Updated", object: await this.produtosService.findOne(id)};
   }
-
+  
   @Delete(':id')
-  @Header('Access-Control-Allow-Origin', 'https://mydomain.com')
+  @Header('Access-Control-Allow-Origin', 'http://localhost:3001/')
+  @Header('Access-Control-Allow-Origin', 'https://teste-react-front.vercel.app/')
   @Header('Access-Control-Allow-Headers', 'Content-Type, Authorization')
   async remove(@Param('id') id: string) {
     return {message: "Object removed successfully", deleted: await this.produtosService.remove(id)};
